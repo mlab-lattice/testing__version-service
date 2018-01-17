8a9dae16952ba899b931959315a8e06be4f2ea3a
Kevin Rosendahl <kevindrosendahl@gmail.com>
2018-01-16 21:34:42 -0800

    version v1.0.0

---
+++ VersionService.js
@@ -0,0 +1,25 @@
+var carbon = require('carbon-io')
+var o  = carbon.atom.o(module).main
+var _o = carbon.bond._o(module)
+var __ = carbon.fibers.__(module).main
+
+__(function() {
+  module.exports = o({
+    _type: carbon.carbond.Service,
+    port: _o('env:PORT') || 8080,
+    endpoints: {
+      version: o({
+        _type: carbon.carbond.Endpoint,
+        get: function(req) {
+          return { version: "v1.0.0" }
+        }
+      }),
+      status: o({
+        _type: carbon.carbond.Endpoint,
+        get: function(req) {
+          return { ok: true }
+        }
+      })
+    }
+  })
+})
